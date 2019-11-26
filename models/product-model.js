49423d1f538435eccf1db4358a450609d0c26ea5
Kunjal kotadia <kuku_2612@Kunjals-MacBook-Air.local>
2019-11-26 18:34:45 -0500

    final commit

--- models/product-model.js
+++ models/product-model.js
@@ -1,16 +1,13 @@
 const mongoose = require('mongoose');
-require('mongoose-double')(mongoose);
 var Schema = mongoose.Schema;
-var SchemaType = mongoose.Schema.Types;
-// var mySchema = new Schema({ double: SchemaTypes.Double });
 let model = new Schema({
     name: {type: String, required: true, max: 100},
     lat: {type: String, required: true},
     long: {type: String, required: true},
-    empTarget: {type: SchemaType.Double, required: true},
-    empTarAch: {type: SchemaType.Double, required: true},
-    contactNo: {type: String, required: true, max:10},
-    salesRatio: {type: SchemaType.Double, required: true }
+    empTarget: {type: Number, required: true},
+    empTarAch: {type: Number, required: true},
+    contactNo: {type: Number, required: true, max:10},
+    salesRatio: {type: Number, required: true }
 
 });
 
